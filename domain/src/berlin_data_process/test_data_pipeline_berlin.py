8b9e4237ff5b5338ca941f0401a5cc798fe7585e
MaxwellMuF <sebastian.rohrscheidt@outlook.com>
2025-01-26 16:27:18 +0100

    TDD_3: RED+GREEN: optimize test, test passed

--- domain/src/berlin_data_process/test_data_pipeline_berlin.py
+++ domain/src/berlin_data_process/test_data_pipeline_berlin.py
@@ -13,41 +13,15 @@ class TestCleaner(unittest.TestCase):
     def setUp(self):
         """Set up all required test data"""
 
-        self.residents_testdata1 = [{'plz': '01109', 
-                                     'note': '01109 Dresden', 
-                                     'einwohner': '20010', 
-                                     'qkm': '24.736193', 
-                                     'lat': '51.12232', 
-                                     'lon': '13.75621'}, 
-                                    {'plz': '01127', 
-                                     'note': '01127 Dresden', 
-                                     'einwohner': '', 
-                                     'qkm': '3.216483', 
-                                     'lat': '51.07897', 
-                                     'lon': '13.72638'},
-                                    {'plz': '01127', 
-                                     'note': '01127 Dresden', 
-                                     'einwohner': '15079', 
-                                     'qkm': '3.216483', 
-                                     'lat': '51.07897', 
-                                     'lon': 'None'},
-                                    {'plz': '01127', 
-                                     'note': '01127 Dresden', 
-                                     'einwohner': '15079', 
-                                     'qkm': 'NaN', 
-                                     'lat': '51.07897', 
-                                     'lon': '13.72638'},
-                                    {'plz': '0', 
-                                     'note': '01127 Dresden', 
-                                     'einwohner': '15079', 
-                                     'qkm': '3.216483', 
-                                     'lat': '51.07897', 
-                                     'lon': '13.72638'}]
         self.residents_reject_data = ["", "None", "NaN", "0"]
+        self.test_data = [{i:values} for i,values in zip(range(len(self.residents_reject_data)),
+                                                             self.residents_reject_data)]
+        self.test_data += [{"key":"some_value"}]
         
     def test_process(self):
         # 1. Case: 1 of 5 rows pass
+        expected = [{"key":"some_value"}]
         cleaner1 = data_pipeline_berlin.Cleaner()
-        self.assertEqual(cleaner1.process(data=self.residents_testdata1, reject_data=self.residents_reject_data), 
-                                            [self.residents_testdata1[0]])
+        self.assertEqual(cleaner1.process(self.test_data, self.residents_reject_data), 
+                         expected)
 
