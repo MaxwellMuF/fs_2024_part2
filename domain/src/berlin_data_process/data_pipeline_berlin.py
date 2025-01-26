953661a5de26ae4550544a058cdbd1cd7b33febc
MaxwellMuF <sebastian.rohrscheidt@outlook.com>
2025-01-26 15:54:10 +0100

    TDD_2 GREEN: pass test

--- domain/src/berlin_data_process/data_pipeline_berlin.py
+++ domain/src/berlin_data_process/data_pipeline_berlin.py
@@ -9,7 +9,7 @@ from typing         import List, Dict, Any
 class Cleaner:
     """Cleans the data by removing rows with missing values."""
     
-    def process(self, data: List[Dict[str, Any]]):
-        cleaned_data = [row for row in data if all(value != "None" and value != "" for value in row.values())]
+    def process(self, data: List[Dict[str, Any]], reject_data: List[str]):
+        cleaned_data = [row for row in data if all(value not in reject_data for value in row.values())]
         print(f"Cleaner: Removed {len(data)-len(cleaned_data)} rows")
         return cleaned_data
